fb978735ca71a7a05ba23855d96d9fb9f5e243eb
jantineschaaf <jantine_141@live.nl>
2025-11-10 11:49:53 +0100

    initial commit

---
+++ src/main/java/nl/miwnn/ch17/tactischetanuki/koala_kitchen_creations/KoalaKitchenCreationsApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.tactischetanuki.koala_kitchen_creations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaKitchenCreationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoalaKitchenCreationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/miwnn/ch17/tactischetanuki/koala_kitchen_creations/KoalaKitchenCreationsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.tactischetanuki.koala_kitchen_creations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoalaKitchenCreationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
