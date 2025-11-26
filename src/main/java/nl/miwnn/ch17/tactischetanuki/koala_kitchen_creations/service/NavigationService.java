4b7be57b2846306a554b52b8039b8a62f632ce39
Josse Muller <jossemuller@live.nl>
2025-11-26 21:06:16 +0100

    Styling changes

--- src/main/java/nl/miwnn/ch17/tactischetanuki/koala_kitchen_creations/service/NavigationService.java
+++ src/main/java/nl/miwnn/ch17/tactischetanuki/koala_kitchen_creations/service/NavigationService.java
@@ -15,8 +15,7 @@ public class NavigationService {
     private static final List<NavItem> navItems = List.of(
             new NavItem("Recipes", "/recipe/all", false),
             new NavItem("Categories", "/category/all", false),
-            new NavItem("Favorites", "/recipe/favorites", true),
-            new NavItem("Users", "/user/all", true)
+            new NavItem("Favorites", "/recipe/favorites", true)
     );
     public List<NavItem> getNavItems(boolean isAuthenticated) {
         if (isAuthenticated) {
